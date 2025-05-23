169c24ff6e80fc42b9c2b956df7ca2aad03e571c
Igor Gabiel Vieira Chaves <contato.igorgabriel@hotmail.com>
2025-05-23 20:09:09 -0300

    🐛 fix: Fix incorrect import/export statement

--- src/store/slice/todoList/index.ts
+++ src/store/slice/todoList/index.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { TODO_LIST_SLICE_CONFIG } from "utils/todoListSlice";
+import { TODO_LIST_SLICE_CONFIG } from "~utils";
 
 export const todoListSlice = createSlice({
     name: TODO_LIST_SLICE_CONFIG.name,
