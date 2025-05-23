85f1a2e43adceb7ceead85889ca57605c50668bc
Igor Gabiel Vieira Chaves <contato.igorgabriel@hotmail.com>
2025-05-23 20:28:56 -0300

    ✨ feat: Create Header component

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,5 +1,19 @@
+import logoSvg from "~assets/svg/logo-lockup.svg";
 import "./styles.scss";
 
 export const HomePage = () => {
-    return <p className="home-page">HomePage</p>;
+    return (
+        <header className="home-page">
+            <img
+                src={logoSvg}
+                alt="Logo"
+                loading="lazy"
+                className="home-page__logo"
+            />
+            <section className="home-page__box">
+                <h1 className="home-page__text home-page__text--title">Stay on top of your tasks</h1>
+                <p className="home-page__text">Manage, track, and complete your tasks with clarity and ease.</p>
+            </section>
+        </header>
+    );
 };
