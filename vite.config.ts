d1001711b8862ee7a24d43756c09a955d9554820
Igor Gabiel Vieira Chaves <contato.igorgabriel@hotmail.com>
2025-05-27 03:46:34 -0300

    ⚙️ config: Absolute import configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,4 +8,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    alias: {
+      "~types": resolve(__dirname, "./src/types")
+    }
+  }
 });
